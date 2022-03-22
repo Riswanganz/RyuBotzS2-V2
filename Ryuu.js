0152ec958292757dbb0a1df306e1da7f4b61485b
Riswanganz <100130164+Riswanganz@users.noreply.github.com>
2022-03-22 08:34:03 +0700

    Add files via upload

--- Ryuu.js
+++ Ryuu.js
@@ -2225,7 +2225,6 @@ latensie = speed() - timestampe
 reply(`_*Speed Test*_\nMerespon dalam ${latensie.toFixed(4)} Sec 💬`)
 break
 case 'tes':
-if (!isUser) return sendButRegis(from, daftar1, daftar2, daftar3, { quoted: ftrol}) 
 if (isBanned) return reply('Kamu Sudah Di banned!')
 tes = fs.readFileSync('./assets/tes.mp3')
 reply('Bot Dah Nyala Bang...')
@@ -2464,62 +2463,93 @@ man = fs.readFileSync('./assets/sound4.mp3');
 Ryuu.sendMessage(from, man, MessageType.audio, {quoted: mek, mimetype: 'audio/mp4', ptt:true})
 break
 
-//Fitur Convert
-case 'gifstiker': case 's': case 'stickergif': case 'sticker': case 'stiker':
-if (!isUser) return sendButRegis(from, daftar1, daftar2, daftar3, { quoted: ftrol}) 
-if (isBanned) return reply('Kamu Sudah Di banned!')
-if ((isMedia && !mek.message.videoMessage || isQuotedImage) && args.length == 0) {
-const encmedia = isQuotedImage ? JSON.parse(JSON.stringify(mek).replace('quotedM', 'm')).message.extendedTextMessage.contextInfo : mek
-const media = await Ryuu.downloadAndSaveMediaMessage(encmedia)
-ran = '666.webp'
-await ffmpeg(`./${media}`)
-.input(media)
-.on('start', function (cmd) {
-console.log(`Started : ${cmd}`)
-})
-.on('error', function (err) {
-console.log(`Error : ${err}`)
-fs.unlinkSync(media)
-reply('error')
-})
-.on('end', function () {
-console.log('Finish')
-Ryuu.sendMessage(from, fs.readFileSync(ran), sticker, {quoted: mek})
-fs.unlinkSync(media)
-fs.unlinkSync(ran)
-})
-.addOutputOptions([`-vcodec`, `libwebp`, `-vf`, `scale='min(320,iw)':min'(320,ih)':force_original_aspect_ratio=decrease,fps=15, pad=320:320:-1:-1:color=white@0.0, split [a][b]; [a] palettegen=reserve_transparent=on:transparency_color=ffffff [p]; [b][p] paletteuse`])
-.toFormat('webp')
-.save(ran)
-} else if ((isMedia && mek.message.videoMessage.seconds < 11 || isQuotedVideo && mek.message.extendedTextMessage.contextInfo.quotedMessage.videoMessage.seconds < 11) && args.length == 0) {
-const encmedia = isQuotedVideo ? JSON.parse(JSON.stringify(mek).replace('quotedM', 'm')).message.extendedTextMessage.contextInfo : mek
-const media = await Ryuu.downloadAndSaveMediaMessage(encmedia)
-ran = '999.webp'
-reply(mess.wait)
-await ffmpeg(`./${media}`)
-.inputFormat(media.split('.')[1])
-.on('start', function (cmd) {
-console.log(`Started : ${cmd}`)
-})
-.on('error', function (err) {
-console.log(`Error : ${err}`)
-fs.unlinkSync(media)
-tipe = media.endsWith('.mp4') ? 'video' : 'gif'
-reply(`Gagal, pada saat mengkonversi ${tipe} ke stiker`)
-})
-.on('end', function () {
-oppaconsole.log('Finish')
-Ryuu.sendMessage(from, fs.readFileSync(ran), sticker, {quoted: mek})
-fs.unlinkSync(media)
-fs.unlinkSync(ran)
-})
-.addOutputOptions([`-vcodec`, `libwebp`, `-vf`, `scale='min(320,iw)':min'(320,ih)':force_original_aspect_ratio=decrease,fps=15, pad=320:320:-1:-1:color=white@0.0, split [a][b]; [a] palettegen=reserve_transparent=on:transparency_color=ffffff [p]; [b][p] paletteuse`])
-.toFormat('webp')
-.save(ran)
-} else {
-reply(`Kirim gambar dengan caption ${prefix}sticker\nDurasi Sticker Video 1-9 Detik`)
-}
-break
+//Fitur Convert recode riswan
+case 'sticker': case 'stikerin':
+				case 'stiker': case 's':{
+						if (!isUser) return sendButRegis(from, daftar1, daftar2, daftar3, { quoted: ftrol}) 
+                        if (isBanned) return reply('Kamu Sudah Di banned!')
+						if (isMedia && !mek.message.videoMessage || isQuotedImage) {
+							const encmedia = isQuotedImage ? JSON.parse(JSON.stringify(mek).replace('quotedM', 'm')).message.extendedTextMessage.contextInfo : mek
+							const media = await Ryuu.downloadAndSaveMediaMessage(encmedia, `./sticker/${sender}`)
+							exif.create('Created By', 'ME-BOT', `stickwm_${sender}`)
+							//ran = getRandom('.webp')
+                        await ffmpeg(`./${media}`)
+                            .input(media)
+                            .on('start', function (cmd) {
+                                console.log(`Started : ${cmd}`)
+                            })
+                            .on('error', function (err) {
+                                console.log(`Error : ${err}`)
+                                fs.unlinkSync(media)
+                                 })
+                            .on('end', async function () {
+                                console.log('Finish')
+                                exec(`webpmux -set exif ./sticker/stickwm_${sender}.exif ./sticker/${sender}.webp -o ./sticker/${sender}.webp`, async (error) => {
+								if (error) return reply(lang.tryAgain())
+								await Ryuu.sendMessage(from, fs.readFileSync(`./sticker/${sender}.webp`), sticker, {quoted: fgif})
+								fs.unlinkSync(media)	
+									fs.unlinkSync(`./sticker/${sender}.webp`)	
+                                    fs.unlinkSync(`./sticker/stickwm_${sender}.exif`)
+								})
+								})
+                           .addOutputOptions([`-vcodec`,`libwebp`,`-vf`,`scale='min(320,iw)':min'(320,ih)':force_original_aspect_ratio=decrease,fps=15, pad=320:320:-1:-1:color=white@0.0, split [a][b]; [a] palettegen=reserve_transparent=on:transparency_color=ffffff [p]; [b][p] paletteuse`])
+							.toFormat('webp')
+							.save(`./sticker/${sender}.webp`)
+                    } else if ((isMedia && mek.message.videoMessage.seconds < 11 || isQuotedVideo && mek.message.extendedTextMessage.contextInfo.quotedMessage.videoMessage.seconds < 11) && args.length == 0) {
+                        const encmedia = isQuotedVideo ? JSON.parse(JSON.stringify(mek).replace('quotedM', 'm')).message.extendedTextMessage.contextInfo : mek
+                        const media = await Ryuu.downloadAndSaveMediaMessage(encmedia)
+                        exif.create('Created By', 'ME-BOT', `stickwm_${sender}`)
+                    //    ran = getRandom('.webp')
+                        await ffmpeg(`./${media}`)
+                            .inputFormat(media.split('.')[1])
+                            .on('start', function (cmd) {
+                                console.log(`Started : ${cmd}`)
+                            })
+                            .on('error', function (err) {
+                                console.log(`Error : ${err}`)
+                                fs.unlinkSync(media)
+                                tipe = media.endsWith('.mp4') ? 'video' : 'gif'
+                                reply(`❌ Gagal, pada saat mengkonversi ${tipe} ke stiker`)
+                            })
+                            .on('end', async function () {
+                                console.log('Finish')
+                                exec(`webpmux -set exif ./sticker/stickwm_${sender}.exif ./sticker/${sender}.webp -o ./sticker/${sender}.webp`, async (error) => {
+						if (error) return reply(lang.tryAgain())
+								await Ryuu.sendMessage(from, fs.readFileSync(`./sticker/${sender}.webp`), sticker, {quoted: fgif})
+								fs.unlinkSync(media)	
+									fs.unlinkSync(`./sticker/${sender}.webp`)	
+                                    fs.unlinkSync(`./sticker/stickwm_${sender}.exif`)
+								})
+								})
+								.addOutputOptions([`-vcodec`,`libwebp`,`-vf`,`scale='min(320,iw)':min'(320,ih)':force_original_aspect_ratio=decrease,fps=15, pad=320:320:-1:-1:color=white@0.0, split [a][b]; [a] palettegen=reserve_transparent=on:transparency_color=ffffff [p]; [b][p] paletteuse`])
+							.toFormat('webp')
+						.save(`./sticker/${sender}.webp`)
+                    } else if ((isMedia || isQuotedImage) && args[0] == 'nobg') {
+                        const encmedia = isQuotedImage ? JSON.parse(JSON.stringify(mek).replace('quotedM', 'm')).message.extendedTextMessage.contextInfo : mek
+                        const media = await Ryuu.downloadAndSaveMediaMessage(encmedia)
+                        ranw = getRandom('.webp')
+                        ranp = getRandom('.png')
+                        reply(mess.wait)
+                        keyrmbg = 'bcAvZyjYAjKkp1cmK8ZgQvWH'
+                        await removeBackgroundFromImageFile({ path: media, apiKey: keyrmbg, size: 'auto', type: 'auto', ranp }).then(res => {
+                            fs.unlinkSync(media)
+                            let bufferir9vn5 = Buffer.from(res.base64img, 'base64')
+                            fs.writeFileSync(ranp, bufferir9vn5, (err) => {
+                                if (err) return reply('Gagal, Terjadi kesalahan, silahkan coba beberapa saat lagi.')
+                            })
+                            exec(`ffmpeg -i ${ranp} -vcodec libwebp -filter:v fps=fps=20 -lossless 1 -loop 0 -preset default -an -vsync 0 -s 512:512 ${ranw}`, (err) => {
+                                fs.unlinkSync(ranp)
+                                if (err) return reply(lang.tryAgain())
+                                Ryuu.sendMessage(from, fs.readFileSync(ranw), sticker, { quoted: mek })
+                                    fs.unlinkSync(ranw)
+                                })
+                            })
+                    } else {
+                        reply(`Kirim gambar dengan caption ${prefix}sticker atau tag gambar yang sudah dikirim\nDurasi sticker video 1-9 detik...`)
+                    }
+                    await limitAdd(sender, limit)
+				            }
+           break
 case 'stickerwm': case 'swm': case 'take': case 'takesticker': case 'takestick':{
 reply(mess.wait) 
 let packname1 = q.split('|')[0] ? q.split('|')[0] : q
